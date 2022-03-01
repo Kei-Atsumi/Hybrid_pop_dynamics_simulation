2dffad89f8bbfce5cc6c3e9d4b7adc7f563191ee
Keisuke <k.atsumi115@gmail.com>
2022-03-01 18:15:16 +0100

    Made config files for each combination of natural selection (genetic incompatibility - BDMI & underdominance)

--- Bash/dis_unlink_disruptive.sbatch.sh
+++ Bash/dis_unlink_disruptive.sbatch.sh
@@ -1,30 +1,57 @@
 #!/bin/bash
 #PBS -N hyb_disruptive_dis_unlink
 #PBS -q blade
-#PBS -l nodes=1:ppn=4,mem=90gb,walltime=48:00:00
+#PBS -l nodes=1:ppn=16,mem=100gb,walltime=48:00:00
 module load gcc
+# Repetition 1 to 100
 REPS=101
-COUNTERA=0
-COUNTERB=0
+# X: first random seed value, its decimal because random seeds are between 0 and 1. If there is an existing one, let X be the newest one.
+# Y: new randmon seed value; let it have the same value as X here
 X=.1356
 Y=.1356
+# COUNTERA, first iteration name; COUNTERB, new iteration name
+COUNTERA=0
+COUNTERB=0
 while [ $COUNTERA -lt $REPS ]; do
  echo "$COUNTERA"
  Y=`echo "$Y + 0.0001" | bc`
- sed -i -e "s/$X/$Y/g" /lustre/k.atsumi/Config/dis_unlink_disruptive_natsel0.1.cfg
- sed -i -e "s/$X/$Y/g" /lustre/k.atsumi/Config/dis_unlink_disruptive_natsel0.3.cfg
- sed -i -e "s/$X/$Y/g" /lustre/k.atsumi/Config/dis_unlink_disruptive_natsel0.7.cfg
- sed -i -e "s/$X/$Y/g" /lustre/k.atsumi/Config/dis_unlink_disruptive_natsel1.cfg
  let COUNTERB=COUNTERB+1
- sed -i -e "s/rep${COUNTERA}dir/rep${COUNTERB}dir/g" /lustre/k.atsumi/Config/dis_unlink_disruptive_natsel0.1.cfg
- sed -i -e "s/rep${COUNTERA}dir/rep${COUNTERB}dir/g" /lustre/k.atsumi/Config/dis_unlink_disruptive_natsel0.3.cfg
- sed -i -e "s/rep${COUNTERA}dir/rep${COUNTERB}dir/g" /lustre/k.atsumi/Config/dis_unlink_disruptive_natsel0.7.cfg
- sed -i -e "s/rep${COUNTERA}dir/rep${COUNTERB}dir/g" /lustre/k.atsumi/Config/dis_unlink_disruptive_natsel1.cfg
- /lustre/k.atsumi/admixem/bin/admixemp /lustre/k.atsumi/Config/dis_unlink_disruptive_natsel0.1.cfg &
- /lustre/k.atsumi/admixem/bin/admixemp /lustre/k.atsumi/Config/dis_unlink_disruptive_natsel0.3.cfg &
- /lustre/k.atsumi/admixem/bin/admixemp /lustre/k.atsumi/Config/dis_unlink_disruptive_natsel0.7.cfg &
- /lustre/k.atsumi/admixem/bin/admixemp /lustre/k.atsumi/Config/dis_unlink_disruptive_natsel1.cfg &
-wait
+ # Change random seed ($X->$Y) and output folder (${COUNTERA}->${COUNTERB})
+ # Run simulation 
+ sed -i -e "s/$X/$Y/g" -e "s/rep${COUNTERA}dir/rep${COUNTERB}dir/g" /lustre/k.atsumi/Config/dis_unlink_disruptive_b0h0.cfg
+ /lustre/k.atsumi/admixem/bin/admixemp /lustre/k.atsumi/Config/dis_unlink_disruptive_b0h0.cfg & 
+ sed -i -e "s/$X/$Y/g" -e "s/rep${COUNTERA}dir/rep${COUNTERB}dir/g" /lustre/k.atsumi/Config/dis_unlink_disruptive_b0h0.1.cfg
+ /lustre/k.atsumi/admixem/bin/admixemp /lustre/k.atsumi/Config/dis_unlink_disruptive_b0h0.1.cfg & 
+ sed -i -e "s/$X/$Y/g" -e "s/rep${COUNTERA}dir/rep${COUNTERB}dir/g" /lustre/k.atsumi/Config/dis_unlink_disruptive_b0h0.4.cfg
+ /lustre/k.atsumi/admixem/bin/admixemp /lustre/k.atsumi/Config/dis_unlink_disruptive_b0h0.4.cfg & 
+ sed -i -e "s/$X/$Y/g" -e "s/rep${COUNTERA}dir/rep${COUNTERB}dir/g" /lustre/k.atsumi/Config/dis_unlink_disruptive_b0h0.8.cfg
+ /lustre/k.atsumi/admixem/bin/admixemp /lustre/k.atsumi/Config/dis_unlink_disruptive_b0h0.8.cfg & 
+ sed -i -e "s/$X/$Y/g" -e "s/rep${COUNTERA}dir/rep${COUNTERB}dir/g" /lustre/k.atsumi/Config/dis_unlink_disruptive_b0.1h0.cfg
+ /lustre/k.atsumi/admixem/bin/admixemp /lustre/k.atsumi/Config/dis_unlink_disruptive_b0.1h0.cfg & 
+ sed -i -e "s/$X/$Y/g" -e "s/rep${COUNTERA}dir/rep${COUNTERB}dir/g" /lustre/k.atsumi/Config/dis_unlink_disruptive_b0.1h0.1.cfg
+ /lustre/k.atsumi/admixem/bin/admixemp /lustre/k.atsumi/Config/dis_unlink_disruptive_b0.1h0.1.cfg & 
+ sed -i -e "s/$X/$Y/g" -e "s/rep${COUNTERA}dir/rep${COUNTERB}dir/g" /lustre/k.atsumi/Config/dis_unlink_disruptive_b0.1h0.4.cfg
+ /lustre/k.atsumi/admixem/bin/admixemp /lustre/k.atsumi/Config/dis_unlink_disruptive_b0.1h0.4.cfg & 
+ sed -i -e "s/$X/$Y/g" -e "s/rep${COUNTERA}dir/rep${COUNTERB}dir/g" /lustre/k.atsumi/Config/dis_unlink_disruptive_b0.1h0.8.cfg
+ /lustre/k.atsumi/admixem/bin/admixemp /lustre/k.atsumi/Config/dis_unlink_disruptive_b0.1h0.8.cfg & 
+ sed -i -e "s/$X/$Y/g" -e "s/rep${COUNTERA}dir/rep${COUNTERB}dir/g" /lustre/k.atsumi/Config/dis_unlink_disruptive_b0.4h0.cfg
+ /lustre/k.atsumi/admixem/bin/admixemp /lustre/k.atsumi/Config/dis_unlink_disruptive_b0.4h0.cfg & 
+ sed -i -e "s/$X/$Y/g" -e "s/rep${COUNTERA}dir/rep${COUNTERB}dir/g" /lustre/k.atsumi/Config/dis_unlink_disruptive_b0.4h0.1.cfg
+ /lustre/k.atsumi/admixem/bin/admixemp /lustre/k.atsumi/Config/dis_unlink_disruptive_b0.4h0.1.cfg & 
+ sed -i -e "s/$X/$Y/g" -e "s/rep${COUNTERA}dir/rep${COUNTERB}dir/g" /lustre/k.atsumi/Config/dis_unlink_disruptive_b0.4h0.4.cfg
+ /lustre/k.atsumi/admixem/bin/admixemp /lustre/k.atsumi/Config/dis_unlink_disruptive_b0.4h0.4.cfg & 
+ sed -i -e "s/$X/$Y/g" -e "s/rep${COUNTERA}dir/rep${COUNTERB}dir/g" /lustre/k.atsumi/Config/dis_unlink_disruptive_b0.4h0.8.cfg
+ /lustre/k.atsumi/admixem/bin/admixemp /lustre/k.atsumi/Config/dis_unlink_disruptive_b0.4h0.8.cfg & 
+ sed -i -e "s/$X/$Y/g" -e "s/rep${COUNTERA}dir/rep${COUNTERB}dir/g" /lustre/k.atsumi/Config/dis_unlink_disruptive_b0.8h0.cfg
+ /lustre/k.atsumi/admixem/bin/admixemp /lustre/k.atsumi/Config/dis_unlink_disruptive_b0.8h0.cfg & 
+ sed -i -e "s/$X/$Y/g" -e "s/rep${COUNTERA}dir/rep${COUNTERB}dir/g" /lustre/k.atsumi/Config/dis_unlink_disruptive_b0.8h0.1.cfg
+ /lustre/k.atsumi/admixem/bin/admixemp /lustre/k.atsumi/Config/dis_unlink_disruptive_b0.8h0.1.cfg & 
+ sed -i -e "s/$X/$Y/g" -e "s/rep${COUNTERA}dir/rep${COUNTERB}dir/g" /lustre/k.atsumi/Config/dis_unlink_disruptive_b0.8h0.4.cfg
+ /lustre/k.atsumi/admixem/bin/admixemp /lustre/k.atsumi/Config/dis_unlink_disruptive_b0.8h0.4.cfg & 
+ sed -i -e "s/$X/$Y/g" -e "s/rep${COUNTERA}dir/rep${COUNTERB}dir/g" /lustre/k.atsumi/Config/dis_unlink_disruptive_b0.8h0.8.cfg
+ /lustre/k.atsumi/admixem/bin/admixemp /lustre/k.atsumi/Config/dis_unlink_disruptive_b0.8h0.8.cfg & 
+ wait
+ Make new random seed and iteration value
  X=`echo "$X + 0.0001" | bc`
  let COUNTERA=COUNTERA+1
-done
+done 
